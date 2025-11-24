7b59359ccc3b2d66d43b10b9435d823751f3cfe0
Guga-CL <gustavo.clima@yahoo.com.br>
2025-11-24 18:14:01 -0300

    ok

--- build.ps1
+++ build.ps1
@@ -9,8 +9,15 @@ if (-not (Test-Path $proj)) {
   exit 1
 }
 
-Write-Host "Building $proj ($Configuration)"
-dotnet build $proj -c $Configuration
+Try{
+
+    Write-Host "Building $proj ($Configuration)"
+    dotnet build $proj -c $Configuration
+    
+}Catch{
+Write-Error "Build failed. Inspect generated files in $OutDir"
+Throw
+}
 
 $buildDir = Join-Path -Path (Split-Path $proj -Parent) -ChildPath "bin\$Configuration\net9.0"
 Write-Host "Build output: $buildDir"
